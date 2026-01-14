607a41135cc7bcbcce8df4e949af519620af0b15
Nicolas14265 <nicolassoaresdos@gmail.com>
2026-01-14 10:25:29 -0300

    adicionar outras aulas

---
+++ Aula1.py
@@ -0,0 +1,37 @@
+print('hello world')
+nome = 'Nicolas'
+idade = 15
+altura = 1.80
+estudante = True
+print('nome:',nome)
+print('idade:',idade)
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudante))
+
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+nota1=7.5
+nota2=8.0
+nota3=6.5
+media = (nota1+nota2+nota3)/3
+print('Média das notas:',media)
+
+nome_usuario = input('Digite seu nome:')
+print('Olá,',nome_usuario)
+
+idade_usuario = input('Digite sua idade:')
+idade_usuario = int(idade_usuario)
+print('Em 5 anos sua idade será', idade_usuario+5)
+
+import math
+print('Raiz quadrada de 16:', math.sqrt(16))
+print('valor de pi:', math.pi)
---
+++ Aula2.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+notas=np.array([7,5,10,8.5,9.5,9])
+media_notas=np.mean(notas)
+maior_nota=np.max(notas)
+
+print(media_notas)
+print(maior_nota)
+
+dados={'nome':['Ana','Beatriz','Carla','Diego'],'idade':[16,17,18,20],'salario':[1000,2000,1515,5000]}
+df=pd.DataFrame(dados)
+sns.barplot(x='nome',y='salario',data=df)
+plt.show()
+
+dados2={'idade':[16,17,18,20],'salario':[1000,2000,1515,5000]}
+df=pd.DataFrame(dados2)
+sns.barplot(x='idade',y='salario',data=df)
+plt.show()
+
+#comparações
+
+maior_de_idade=df['idade'][3]>=18
+bom_salario=df['salario'][3]>=3000
+
+#lógica combinada
+
+adulto_independente=maior_de_idade and bom_salario
+joven_dependente=(not maior_de_idade) and (not bom_salario)
+
+#classificações
+
+if adulto_independente:
+    print('adulto bem de vida')
+elif joven_dependente:
+    print('joven sem dinheiro')
+else:
+    print('nenhuma das opções')
